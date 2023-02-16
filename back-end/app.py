92cf84ede7aafda77a1842314d3a1f68f13ef976
Mark Perrie <mark.perrie@and.digital>
2023-02-16 13:54:32 +0000

    redo lost work, version control is cool

--- back-end/app.py
+++ back-end/app.py
@@ -1,20 +1,20 @@
-from flask import Flask, request
-from flask_mail import Mail
+from flask import Flask, request, jsonify
+from flask_mail import Mail, Message 
 import os
 from dotenv import load_dotenv
 from flask_cors import CORS
 
 
 app = Flask("Unit Test Server")
-CORS(app)
 load_dotenv()
-app.config['MAIL_SERVER']='smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
-app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
+CORS(app)
 
 @app.route("/")
 def heartbeat():
@@ -22,20 +22,25 @@ def heartbeat():
         "greeting": "Hello World. The grass is green, the sun is shining and the server is alive!"
     }
 
-@app.route("/email")
+
+@app.route("/email", methods=['POST'])
 def sendEmail():
-  args = request.args
-  msg = Message(args.message, sender=args.from)
-  
-  mail.send(msg)
-  return "Message sent!"
+    email = request.args.get('email')
+    subject = request.args.get('subject')
+    message = request.args.get('email')
+    
+    msg = Message(subject, sender = os.environ.get('EMAIL_USER'), recipients = [email])
+    msg.body = message
+    mail.send(msg)
+    return jsonify("Sent")
+
 
 @app.route("/shopping")
 def getShoppingList():
     return {
         "shoppingList": ["Malteasers", "Butter", "Oat Milk", "Almond Milk", "Old El Paso Fajita Mix", "Store Brand Tortillas", "Bread", "Chicken Stock", "Beef Stock", "Vegetable Stock", "Fish Stock", "Unimaginative Stock"]
     }
-    
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
